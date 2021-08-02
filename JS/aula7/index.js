7de8199b84f57cafe221164b8000c41390fce866
Romalua <72881157+Romalua@users.noreply.github.com>
2021-08-02 14:22:41 -0300

    save

---
+++ JS/aula7/index.js
@@ -0,0 +1,12 @@
+const nome = 'João';
+
+/*
+Não criar variáveis com palavras reservadas
+Variáveis precisam ter nomes significativos 
+Não pode começar com um numero de
+Não podem conter espaços ou traços
+Utilizar camelCase
+Case-sensitive
+Não podemos redeclarar variaveis em letter
+NÃO UTILIZE VAR , USE CONST
+*/
