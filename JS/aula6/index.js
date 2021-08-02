a97ce0a436fbec8a538cb437e149670ac8dec130
Lucas Barreto <72881157+Romalua@users.noreply.github.com>
2021-08-02 03:04:10 -0300

    save

--- JS/aula6/index.js
+++ JS/aula6/index.js
@@ -1,3 +1,12 @@
 let nome = "Lucas"; // String 
-
 console.log(nome, 'nasceu em 1984');
+
+/*
+Não criar variáveis com palavras reservadas
+Variáveis precisam ter nomes significativos 
+Não pode começar com um numero de
+Não podem conter espaços ou traços
+Utilizar camelCase
+Case-sensitive
+Não podemos redeclarar variaveis em letter
+*/
