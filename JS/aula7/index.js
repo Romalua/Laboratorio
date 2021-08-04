897f734d511bb3746214ddf1c4de7e577df7beff
Romalua <72881157+Romalua@users.noreply.github.com>
2021-08-04 08:07:09 -0300

    save

--- JS/aula7/index.js
+++ JS/aula7/index.js
@@ -24,5 +24,4 @@ const resultadoDuplicado = resultado * 2;
 let resultadoTriplicado = resultado *3; 
 resultadoTriplicado = resultadoTriplicado + 5;
 console.log(resultadoTriplicado);
-
 console.log(primeiroNumero + segundoNumero);
