fcb7b965c93952446f4ba971dbe9866767c99cbe
Romalua <72881157+Romalua@users.noreply.github.com>
2021-08-04 09:45:04 -0300

    save

---
+++ JS/aula8/index.js
@@ -0,0 +1,10 @@
+/*
+Luiz Otávio Miranda tem 30 anos. pesa 84 KG
+tem 1.80 de altura  e seu imc é de 25.82
+*/
+const nome = "Lucas André";
+const idade = 23;
+let peso = 65 ;
+const altura = 1.80;
+let imc =  peso/(altura*altura);
+console.log(nome,'tem',idade,'anos e pesa',peso,'KG. Ele tem de altura ',altura,'m e seu imc é de ',imc,)
