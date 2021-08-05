0f990c950044e97ee107a918fc92b1b83c8a2df5
Romalua <72881157+Romalua@users.noreply.github.com>
2021-08-05 13:31:29 -0300

    save

---
+++ JS/aula10/index.js
@@ -0,0 +1,7 @@
+const nome  = 'Luiz';//srting 
+const nome2 = "Luiz";//srting 
+const nome3 = `Luiz`;//srting 
+const num1 = 10; //number 
+const num2 = 10.52; //number
+let nomeAluno; //undefined -> não aponta pra local na memória
+const sobrenimeAluno = null; // Nulo -> não aponta pra local na memória
