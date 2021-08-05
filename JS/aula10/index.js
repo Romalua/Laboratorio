9d5f779b148c9214cae13e88f768f4ed964b78b1
Romalua <72881157+Romalua@users.noreply.github.com>
2021-08-05 13:42:49 -0300

    save

--- JS/aula10/index.js
+++ JS/aula10/index.js
@@ -5,3 +5,13 @@ const num1 = 10; //number
 const num2 = 10.52; //number
 let nomeAluno; //undefined -> não aponta pra local na memória
 const sobrenimeAluno = null; // Nulo -> não aponta pra local na memória
+const aprovado = false;// Boolean = verdade, falso (lógico)
+
+/*
+const a = [1, 2];
+const b = a;
+
+console.log(a, b);
+b.push(3);
+console.log(a, b)
+*/
