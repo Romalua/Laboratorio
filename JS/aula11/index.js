6b0312461ef0883a75d52ae1e3a0514de07361ae
Lucas Barreto <72881157+Romalua@users.noreply.github.com>
2021-08-08 22:59:11 -0300

    save

--- JS/aula11/index.js
+++ JS/aula11/index.js
@@ -1,1 +1,5 @@
-window.alert ('Mensagem')
+window.alert ('Mensagem');
+window.confirm('Deseja realmente apagar ? ');
+window.prompt("Digite seu nome: ");
+var nome = window.prompt('Seu nome de novo: ');
+window.alert(`Seu nome é ${nome}`);
