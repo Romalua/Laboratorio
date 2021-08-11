b25f5e764135bf2ef4fd155dfad9a785d1b2be8e
Romalua <72881157+Romalua@users.noreply.github.com>
2021-08-11 10:39:57 -0300

    save

--- JS/aula11/index.js
+++ JS/aula11/index.js
@@ -6,11 +6,14 @@ var nome = window.prompt('Seu nome de novo: ');
 window.alert(`Seu nome é ${nome}`);
 */
 
-const num1 = prompt('Digite o primeiro valor: ');
-const num2 = prompt('Digite o segundo valor: ');
-const num3 = prompt('Digite o terceiro valor: ');
+let num1 = prompt('Digite o primeiro valor: ');
+let num2 = prompt('Digite o segundo valor: ');
+let num3 = prompt('Digite o terceiro valor: ');
+
 num1 = Number(num1);
 num2 = Number(num2);
 num3 = Number(num3);
 
-window.alert(`Resultado é ${num1 + num2 + num3}`);
+resultado = num1 + num2 + num3
+
+window.alert(`Resultado é ${resultado}`);
