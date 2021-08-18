bddd26405bb5b7553d8d535d7481cdec2d508626
Romalua <72881157+Romalua@users.noreply.github.com>
2021-08-18 16:58:53 -0300

    save

--- JS/aula13/aula.js
+++ JS/aula13/aula.js
@@ -1,6 +1,11 @@
-let umaString = "um texto";
+let umaString = "Um texto";
 //Concatenação//--
-
-console.log(umaString.concat('em um dia.'));
-console.log(umaString + 'em um dia.');
+/*
+console.log(umaString.concat(' em um dia.'));
+console.log(umaString + ' em um dia.');
 console.log(`${umaString} em um dia.`);
+*/
+console.log(umaString.lastIndexOf('m',3));
+console.log(umaString.indexOf('o', 3));
+console.log(umaString.match(/[a-z]/));
+console.log(umaString.search(/[a-z]/));
