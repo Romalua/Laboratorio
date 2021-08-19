537cce19636e0e109b4c29b64b6f2e2af46b3ec9
Romalua <72881157+Romalua@users.noreply.github.com>
2021-08-19 11:36:44 -0300

    save

---
+++ Python/Coder/Lista01/ex003.py
@@ -0,0 +1,5 @@
+"""Faça um Programa que peça dois números e imprima a soma."""
+num1 = float(input("Valor 1: "))
+num2 = float(input("Valor 2: "))
+
+print ("A soma entre {} e {} é {}".format(num1, num2, num1+num2))
