180dc926e4b4e990c20c095e3a4c76f225d74dfa
Romalua <72881157+Romalua@users.noreply.github.com>
2021-08-19 10:01:34 -0300

    save

---
+++ JS/LISTA/index1.js
@@ -0,0 +1,7 @@
+let nota1;
+nota1 = prompt('Digite o primeiro valor: ');
+//como o vscode recomendo que fosse 
+
+let num1 = prompt('Digite o primeiro valor: ');
+//como eu coloquei inicialmente a declaração
+
