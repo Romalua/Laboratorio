ed029c4825b8f41f5c195602d287bc103aafda5e
Romalua <72881157+Romalua@users.noreply.github.com>
2021-08-19 10:56:55 -0300

    save

--- JS/LISTA/index1.js
+++ JS/LISTA/index1.js
@@ -1,7 +1,5 @@
-let nota1;
-nota1 = prompt('Digite o primeiro valor: ');
-//como o vscode recomendo que fosse 
-
-let num1 = prompt('Digite o primeiro valor: ');
+const nota1 = prompt('Nota do Primeiro Trismestre : ');
 //como eu coloquei inicialmente a declaração
+const nota2 = prompt('Nota do Segundo Trimestre :');
+const nota3 = prompt('Nota do Terceiro Trimestre :');
 
