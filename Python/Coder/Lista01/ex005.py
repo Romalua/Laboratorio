0efc0d7c79c4d387e3a5f7841e3c6c38c89d1654
Romalua <72881157+Romalua@users.noreply.github.com>
2021-08-19 11:41:52 -0300

    save

---
+++ Python/Coder/Lista01/ex005.py
@@ -0,0 +1,5 @@
+"""Faça um Programa que converta metros para centímetros."""
+
+metros = float(input("Valor em Metros: "))
+centimetros = (metros*100)
+print ("{:.0f} metros convertidos para CM fica {:.0f} centimetros".format(metros, centimetros))70
