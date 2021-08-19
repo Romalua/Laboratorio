8fade6e576263c4ad1539eedba84018c2fc8d53f
Romalua <72881157+Romalua@users.noreply.github.com>
2021-08-19 11:26:22 -0300

    save

---
+++ Python/Coder/Lista01/ex002.py
@@ -0,0 +1,4 @@
+"""Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número]."""
+
+number = float(input("Number Digitado: "))
+print("Numero digitado foi {:.2f}".format(number))
