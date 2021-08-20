42fcc1a2f90b45d9f18a53544d6a036aa2b57f38
Romalua <72881157+Romalua@users.noreply.github.com>
2021-08-20 13:25:03 -0300

    save

--- Python/Coder/Lista01/ex008.py
+++ Python/Coder/Lista01/ex008.py
@@ -1,4 +1,7 @@
 """Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas
    no mês. Calcule e mostre o total do seu salário no referido mês."""
    
-   
+
+money_hora = int(input("Valor recebido por hora: "))
+mes_hora =  int (input("Número de horas trabalhadas no mês: "))
+print ("Total do seu salário do mês: {}".format(money_hora*mes_hora))
