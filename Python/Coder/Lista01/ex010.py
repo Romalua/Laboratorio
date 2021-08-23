c7750d26d60d00a3b4cf62354a7ca0afc2c8d2d1
Romalua <72881157+Romalua@users.noreply.github.com>
2021-08-23 14:12:28 -0300

    save

--- Python/Coder/Lista01/ex010.py
+++ Python/Coder/Lista01/ex010.py
@@ -1,4 +1,6 @@
 """Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre 
    em graus Fahrenheit. (27 °C × 9/5) + 32 = 80,6 °F """
    
-print()
+celsius = int(input("Temperatura em Grau Celsius: "))
+fah = (celsius * (9/5)) + 32
+print ("A temperatura de {}°C é de {:.0f}°F".format(celsius, fah))
