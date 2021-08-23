5e2cb57701ac4f35fa20431c93d871504b5213a8
Romalua <72881157+Romalua@users.noreply.github.com>
2021-08-23 15:54:46 -0300

    save

--- Python/Coder/Lista01/ex011.py
+++ Python/Coder/Lista01/ex011.py
@@ -3,7 +3,7 @@ Faça um Programa que peça 2 números inteiros e um número real. Calcule e mos
     o produto do dobro do primeiro com metade do segundo .
     a soma do triplo do primeiro com o terceiro.
     o terceiro elevado ao cubo"""
-    
+
 number1 = int(input("1° Numero é Inteiro: "))
 number2 = int(input("2° Numero é Inteiro: "))
 number3 = float(input("3° Numero é Real: "))
@@ -12,6 +12,8 @@ soluc1 = (number1*2) + (number2/2)
 soluc2 = (number1*3) + number3
 soluc3 = (number3 ** 3)
 
-print ("O produto do dobro de {:.0F} com metade de {:.0F} é {:.0F}".format(number1,number2, soluc1))
-print ("A soma do triplo de {:.0F} com o valor de {:.0F} é {:.0F}".format(number1,number3, soluc2))
-print ("O valor de {:.0F} elevado ao cubo é {:.0F}".format(number3,soluc3))
+print("O produto do dobro de {:.0F} com metade de {:.0F} é {:.0F}".format(
+    number1, number2, soluc1))
+print("A soma do triplo de {:.0F} com o valor de {:.0F} é {:.0F}".format(
+    number1, number3, soluc2))
+print("O valor de {:.0F} elevado ao cubo é {:.0F}".format(number3, soluc3))
