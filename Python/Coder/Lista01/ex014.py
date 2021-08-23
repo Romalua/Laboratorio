4a127177a46c4485c89403bf5902b4283811ab0c
Romalua <72881157+Romalua@users.noreply.github.com>
2021-08-23 16:24:20 -0300

    save

---
+++ Python/Coder/Lista01/ex014.py
@@ -0,0 +1,23 @@
+    """João Papo-de-Pescador, homem de bem, comprou um microcomputador para controlar o
+    rendimento diário de seu trabalho. Toda vez que ele traz um peso de peixes maior que
+    o estabelecido pelo regulamento de pesca do estado de São Paulo (50 quilos) deve
+    pagar uma multa de R$ 4,00 por quilo excedente. João precisa que você faça um
+    programa que leia a variável peso (peso de peixes) e calcule o excesso. Gravar na
+    variável excesso a quantidade de quilos além do limite e na variável multa o valor
+    da multa que João deverá pagar. Imprima os dados do programa com as mensagens
+    adequadas."""
+
+
+peso  = float(input("Quantidade de Kg: "))
+
+    if peso = <50
+    
+
+
+    
+    
+    
+    """peso 
+    peso passa de 50
+    cada kilo vale 4,00 de multa 
+    """
