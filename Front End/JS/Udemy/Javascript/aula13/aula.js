520f24cf2345d0c746778424c6864359a240d460
Lucas Barreto <72881157+Romalua@users.noreply.github.com>
2021-08-26 19:56:25 -0300

    save

--- Front End/JS/Udemy/Javascript/aula13/aula.js
+++ Front End/JS/Udemy/Javascript/aula13/aula.js
@@ -9,3 +9,4 @@ console.log(umaString.lastIndexOf('m',3));
 console.log(umaString.indexOf('o', 3));
 console.log(umaString.match(/[a-z]/));
 console.log(umaString.search(/[a-z]/));
+console.log          ,                                   
