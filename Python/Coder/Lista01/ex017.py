5142d1eff36b2c1db452725ded8a74ec6476654f
Romalua <72881157+Romalua@users.noreply.github.com>
2021-08-31 11:00:54 -0300

    save

--- Python/Coder/Lista01/ex017.py
+++ Python/Coder/Lista01/ex017.py
@@ -9,3 +9,5 @@ comprar apenas galões de 3,6 litros;
 misturar latas e galões, de forma que o desperdício de tinta seja menor. Acrescente 10% de
 folga e sempre arredonde os valores para cima, isto é, considere latas cheias."""
 
+metro-area = float (
+        input("Tamanho em metros quadrados da área a ser pintada: "))
