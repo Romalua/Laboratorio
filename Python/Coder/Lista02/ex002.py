f6f3cdd5bab5631ca9560debbb34cb3f0ec9808b
Romalua <72881157+Romalua@users.noreply.github.com>
2021-08-31 16:24:13 -0300

    save

---
+++ Python/Coder/Lista02/ex002.py
@@ -0,0 +1,8 @@
+"""Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo."""
+
+number = float (input("Digite um numero: "))
+
+if number>0:
+    print ("Numero é positivo")
+else:
+    print ("Numero é negativo")    
