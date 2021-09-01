3c403eb9bb358b7202a34aa9ff9b8ceec9bd802c
Romalua <72881157+Romalua@users.noreply.github.com>
2021-09-01 18:00:26 -0300

    save

--- Python/Coder/Lista02/ex011.py
+++ Python/Coder/Lista02/ex011.py
@@ -2,9 +2,9 @@
 e lhe contraram para desenvolver o programa que calculará os reajustes.
     Faça um programa que recebe o salário de um colaborador e o reajuste segundo o seguinte 
     critério, baseado no salário atual:
-    -salários até R$ 280,00 (incluindo) : aumento de 20%
-    -salários entre R$ 280,00 e R$ 700,00 : aumento de 15%
-    -salários entre R$ 700,00 e R$ 1500,00 : aumento de 10%
+   // -salários até R$ 280,00 (incluindo) : aumento de 20%
+   // -salários entre R$ 280,00 e R$ 700,00 : aumento de 15%
+   // -salários entre R$ 700,00 e R$ 1500,00 : aumento de 10%
     -salários de R$ 1500,00 em diante : aumento de 5% Após o aumento ser realizado,
     informe na tela:
     -o salário antes do reajuste;
@@ -23,9 +23,52 @@ if money <= 280:
     print("-=-" * 20)
     print("Processando...")
     sleep(1)
-    print("\nSalário antes do reajuste R${}".format(money))
+    print("\nSalário antes do reajuste R${:.2f}".format(money))
     print("\nPercentual de aumento aplicado - 20%")
-    print("\nValor do aumento R${}".format(ajuste))
-    print("\nNovo salário R${}".format(aumento))
+    print("\nValor do aumento R${:.2f}".format(ajuste))
+    print("\nNovo salário R${:.2f}".format(aumento))
     print('-=-' * 20)
     print('\n')
+
+elif money == 281 or money <= 700:
+    ajuste = (money * 0.15)
+    aumento = money + ajuste
+    print("\n")
+    print("-=-" * 20)
+    print("Processando...")
+    sleep(1)
+    print("\nSalário antes do reajuste R${:.2f}".format(money))
+    print("\nPercentual de aumento aplicado - 15%")
+    print("\nValor do aumento R${:.2f}".format(ajuste))
+    print("\nNovo salário R${:.2f}".format(aumento))
+    print('-=-' * 20)
+    print('\n')
+
+elif money == 700 or money <= 1500:
+    ajuste = (money * 0.10)
+    aumento = money + ajuste
+    print("\n")
+    print("-=-" * 20)
+    print("Processando...")
+    sleep(1)
+    print("\nSalário antes do reajuste R${:.2f}".format(money))
+    print("\nPercentual de aumento aplicado - 10%")
+    print("\nValor do aumento R${:.2f}".format(ajuste))
+    print("\nNovo salário R${:.2f}".format(aumento))
+    print('-=-' * 20)
+    print('\n')
+
+elif money > 1500:
+    ajuste = (money * 0.05)
+    aumento = money + ajuste
+    print("\n")
+    print("-=-" * 20)
+    print("Processando...")
+    sleep(1)
+    print("\nSalário antes do reajuste R${:.2f}".format(money))
+    print("\nPercentual de aumento aplicado - 5%")
+    print("\nValor do aumento R${:.2f}".format(ajuste))
+    print("\nNovo salário R${:.2f}".format(aumento))
+    print('-=-' * 20)
+    print('\n')
+    
