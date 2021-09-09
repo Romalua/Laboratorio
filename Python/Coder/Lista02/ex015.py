604ed785fe8333845ce19f43d27683793e46f2d3
Romalua <72881157+Romalua@users.noreply.github.com>
2021-09-09 08:38:58 -0300

    safe

--- Python/Coder/Lista02/ex015.py
+++ Python/Coder/Lista02/ex015.py
@@ -7,3 +7,16 @@ Três lados formam um triângulo quando a soma de quaisquer dois lados for maior
 Triângulo Equilátero: três lados iguais;
 Triângulo Isósceles: quaisquer dois lados iguais;
 Triângulo Escaleno: três lados diferentes;"""
+
+lado1 = float(input("Digite o valor do lado1 do triângulo: "))
+lado2 = float(input("Digite o valor do lado2 do triângulo: "))
+lado3 = float(input("Digite o valor do lado3 do triângulo: "))
+
+if (lado1 + lado2 < lado3) or (lado1 + lado3 < lado2) or (lado2 + lado3 < lado1):
+    print("Não é triangulo")
+elif (lado1== lado2) and (lado1 == lado3):
+    print("Triângulo Equilátero")
+elif (lado1== lado2) or (lado2 == lado3) or (lado1== lado3):
+    print("Triângulo Isósceles")
+else:
+    print('Escaleno')
