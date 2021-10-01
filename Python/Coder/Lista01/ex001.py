9df732d6b8caf28a6988276212e952828dc64cce
Lucas Barreto <72881157+Romalua@users.noreply.github.com>
2021-10-01 13:57:13 -0300

    save

--- Python/Coder/Lista01/ex001.py
+++ Python/Coder/Lista01/ex001.py
@@ -1,3 +1,14 @@
-"""Faça um Programa que mostre a mensagem "Alo mundo" na tela."""
+idade = int(input("Insira a sua idade:"))
 
-print("Alô Mundo")
+if(idade >= 5 and idade <= 7):
+    print("Sua categoria é infantil A")
+elif(idade >= 8 and idade <= 10):
+    print("Sua categoria é infantil B")
+elif(idade >= 11 and idade <= 13):
+    print("Sua categoria é juventil A")
+elif(idade >= 14 and idade <= 17):
+    print("Sua categoria é juventil B")
+elif(idade >= 18):
+    print("Sua categoria é Adulto")
+else:
+    print("idade não permitida")
