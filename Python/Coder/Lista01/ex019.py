eb4f397d8828681381ba93b4d5d8217481e52c96
Romalua <72881157+Romalua@users.noreply.github.com>
2021-11-09 17:46:05 -0300

    save

---
+++ Python/Coder/Lista01/ex019.py
@@ -0,0 +1,6 @@
+nome = input("Informe o usuario: ")
+senha = input("Informe sua senha: ")
+if (nome == "Lucas" and senha == "171998"):
+    print("Acesso permitido")
+else:
+    print("Acesso Negado!!!")
