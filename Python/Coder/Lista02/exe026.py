9fae452149b48b9c862fda934d8cf685189dfc14
Romalua <72881157+Romalua@users.noreply.github.com>
2021-11-11 11:44:13 -0300

    save

--- Python/Coder/Lista02/exe026.py
+++ Python/Coder/Lista02/exe026.py
@@ -11,3 +11,39 @@ o tipo de combustível (codificado da seguinte forma: A-álcool, G-gasolina),
 calcule e imprima o valor a ser
 pago pelo cliente sabendo-se que o preço do litro da gasolina é R$ 6,49 o 
 preço do litro do álcool é R$ 5,49."""
+
+Com = input("Informe o típo de combustível A-álcool, G-gasolina: ")
+
+if Com == "A":
+
+    QA = int(input('Informe a quantidade de Alcool em litros a ser comprada: '))
+    if QA <= 20:
+        A = "Álcool"
+        bruto = QA * 5.49
+        desconto = (bruto*0.03)
+        liquido = bruto - desconto
+        print("Você escolher abastercer com {}, com a quantidade de {} litros, com desconto de 3% que da {:.2F}R, você ira pagar {:.2f}R$ no final".format(A, QA, liquido))
+    elif QA > 20:
+        A = "Álcool"
+        bruto = QA * 5.49
+        desconto = (bruto*0.05)
+        liquido = bruto - desconto
+        print("Você escolher abastercer com {}, com a quantidade de {} litros, com desconto de 3% que da {:.2F}R, você ira pagar {:.2f}R$ no final".format(A, QA, liquido))
+
+elif Com == "G":
+
+    QG = int(input('Informe a quantidade de Gasolina em litros a ser comprada: '))
+    if QG <= 20:
+        G = "Gasolina"
+        bruto = QG * 6.49
+        desconto = (bruto*0.04)
+        liquido = bruto - desconto
+        print("Você escolher abastercer com {}, com a quantidade de {} litros, com desconto de 4% que da {:.2F}R, você ira pagar {:.2f}R$ no final".format(G,QG,desconto,liquido))    
+    elif QG > 20:
+        G = "Gasolina"
+        bruto = QG * 6.49
+        desconto = (bruto*0.06)
+        liquido = bruto - desconto
+        print("Você escolher abastercer com {}, a quantidade de {} litros, com desconto de 6% que da {:.2F}R$, você ira pagar {:.2f}R$ no final".format(G,QG,desconto,liquido))      
+else:
+    print("Opçaõ Invalida!")             
