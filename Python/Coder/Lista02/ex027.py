9e795e31b60fe5988854795d06b384e4780c44b9
Romalua <72881157+Romalua@users.noreply.github.com>
2021-11-25 16:44:51 -0300

    save

--- Python/Coder/Lista02/ex027.py
+++ Python/Coder/Lista02/ex027.py
@@ -7,13 +7,22 @@ R$ 25,00, receberá ainda um desconto de 10% sobre este total. Escreva um algori
 para ler a quantidade (em Kg) de morangos e a quantidade (em Kg)
 de maças adquiridas e escreva o valor a ser pago pelo cliente."""
 
-from time import sleep
 
-print("""--- Lista de Frutas disponiveis ---
-                Até 5 Kg                Acima de 5 Kg
-Morango         R$ 2,50 por Kg          R$ 2,20 por Kg
-Maçã            R$ 1,80 por Kg          R$ 1,50 por Kg""")
+morango = float(input("Digite quantos quilos de morango foram comprados: "))
+maca = float(input("Digite quantos quilos de maçã foram comprados: "))
+valor = 0
 
-morango = input("Vai querer morango? Sim ou Não:")
+if morango <= 5:
+    valor += morango * 2.5
+else:
+    valor += morango * 2.2
+if maca <= 5:
+    valor += maca * 1.8
+else:
+    valor += maca * 1.5
 
+if (morango + maca) > 8 or valor > 25:
+    valor -= valor * 10 / 100
+
+print(f"O valor a ser pago é R${valor:.2f}")
    
