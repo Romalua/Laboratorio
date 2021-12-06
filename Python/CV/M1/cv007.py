629988e25e82bde6050d96e5adb48ee2c4440afb
Lucas Barreto <72881157+Romalua@users.noreply.github.com>
2021-12-06 20:55:58 -0300

    save

--- Python/CV/M1/cv007.py
+++ Python/CV/M1/cv007.py
@@ -2,4 +2,4 @@ nt1 = int(input("Digite a primeira nota: "))
 nt2 = int(input("Digite a segunda nota: "))
 media = (nt1 + nt2)/2
 
-print("A média de {} e {} é igual {}".format(nt1,nt2,media))
+print("A média de {} e {} é igual {}".format(nt1, nt2, media))
