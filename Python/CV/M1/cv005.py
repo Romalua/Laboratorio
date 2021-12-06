c2699bcfd9af968eab528b7d3bc616d7002cd5b1
Lucas Barreto <72881157+Romalua@users.noreply.github.com>
2021-12-06 20:31:58 -0300

    save

---
+++ Python/CV/M1/cv005.py
@@ -0,0 +1,4 @@
+valor1 = float(input("Digite um numero: "))
+antecessor = valor1 - 1 
+sucessor = valor1 + 1
+print("Analisando o valor {:.0f} seu antecessor é {:.0f} e p sucessor é {:.0f}".format(valor1,antecessor,sucessor))
