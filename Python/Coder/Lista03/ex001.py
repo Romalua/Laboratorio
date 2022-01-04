6bd45ec46237b0f849d4792c0a9dce0ee1da244f
Romalua <72881157+Romalua@users.noreply.github.com>
2022-01-04 11:19:31 -0300

    save

--- Python/Coder/Lista03/ex001.py
+++ Python/Coder/Lista03/ex001.py
@@ -3,4 +3,4 @@ e continue pedindo até que o usuário informe um valor válido."""
 
 nota = float(input("informe um numero de 0 a 10: "))
 while (nota > 10) or (nota < 0):
-    nota = float(input("informe um numero de 0 a 10: "))
+   nota = float(input("informe um numero de 0 a 10: "))
