909bf5c88d6154b92f7ab3519855e490d08de9a5
Romalua <72881157+Romalua@users.noreply.github.com>
2022-01-24 17:29:52 -0300

    save

--- Python/Coder/Lista03/ex003.py
+++ Python/Coder/Lista03/ex003.py
@@ -4,3 +4,11 @@ b Idade: entre 0 e 150;
 c Salário: maior que zero;
 d Sexo: 'f' ou 'm';
 e Estado Civil: 's', 'c', 'v', 'd';"""
+
+nome = str(input("informe um nome--> "))
+while (len(nome) <= 3):
+    nome = str(input("informe um nome--> "))
+    
+idade =int(input(input("Idade: ")))
+while ( idade > 150 or idade < 0 ):
+    idade=int(input("informe a idade--> "))
