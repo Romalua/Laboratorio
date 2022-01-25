02031a4630fda8b8528a617f912e9bd09ad25646
Romalua <72881157+Romalua@users.noreply.github.com>
2022-01-25 09:43:54 -0300

    save

--- Python/Coder/Lista03/ex005.py
+++ Python/Coder/Lista03/ex005.py
@@ -1,8 +1,2 @@
 """Altere o programa anterior permitindo ao usuário informar as populações e as taxas de crescimento iniciais.
 Valide a entrada e permita repetir a operação."""
-
-def calculo ():
-    povoA = float(input("Numero da população do Pais A: "))
-    taxaA = float(input("Taxa de crescimento por ano do Pais A: "))
-    povoB = float(input("Numero da população do Pais B: "))
-    taxaB = float(input("Taxa de crescimento por ano do Pais B: "))
