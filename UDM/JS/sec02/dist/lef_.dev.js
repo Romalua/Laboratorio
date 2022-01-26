7a3daab94461176d27662994dcebf9b7d9382faf
Romalua <72881157+Romalua@users.noreply.github.com>
2022-01-26 10:56:43 -0300

    save

---
+++ UDM/JS/sec02/dist/lef_.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var nome;
+nome = 'Barretin o Brabo Delas';
+console.log(nome);
+nome = "Lucas";
+console.log(nome);
---
+++ UDM/JS/sec02/lef_.js
@@ -0,0 +1,5 @@
+let nome;
+nome = 'Barretin o Brabo Delas'
+console.log(nome);
+nome = "Lucas";
+console.log(nome);
