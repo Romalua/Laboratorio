4de4140817d449681c8b74a708ca4aabc9d982de
Romalua <72881157+Romalua@users.noreply.github.com>
2022-01-26 15:01:40 -0300

    save

---
+++ UDM/JS/sec02/_05dadosPrimitivos.js
@@ -0,0 +1,5 @@
+//string 'nome'  "nome" `nome`
+// number 10 10.52 
+let nomeAluno; //undefined = não aponta pra lugar nenhum
+const sobreAluno = null;  //não aponta pra lugar nenhum
+const aprovado = False; // Boolean = True, False (lógico)
---
+++ UDM/JS/sec02/dist/_05dadosPrimitivos.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//string 'nome'  "nome" `nome`
+// number 10 10.52 
+var nomeAluno; //undefined = não aponta pra lugar nenhum
+
+var sobreAluno = null; //não aponta pra lugar nenhum
+
+var aprovado = False; // Boolean = True, False (lógico)
