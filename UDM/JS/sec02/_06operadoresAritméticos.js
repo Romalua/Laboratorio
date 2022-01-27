1952857b9cf0f582de71e4d7cd5245ce0d1ab7a1
Romalua <72881157+Romalua@users.noreply.github.com>
2022-01-27 09:48:40 -0300

    savesave

---
+++ UDM/JS/sec02/_06operadoresAritméticos.js
@@ -0,0 +1,14 @@
+/*
++  adição/ concatenação
+-  subtração
+/  divisão
+*  multiplicação
+
+*/
+const num = 5;
+const num2 = 10;
+
+console.log(num + num2);
+console.log(num - num2);
+console.log(num / num2);
+console.log(num * num2);
---
+++ UDM/JS/sec02/dist/_06operadoresAritméticos.dev.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/*
++  adição/ concatenação
+-  subtração
+/  divisão
+*  multiplicação
+
+*/
+var num = 5;
+var num2 = 10;
+console.log(num + num2);
+console.log(num - num2);
+console.log(num / num2);
+console.log(num * num2);
