dff073171dd46eb0254b56cdcec5109b819e23c6
Romalua <72881157+Romalua@users.noreply.github.com>
2022-02-03 09:41:14 -0300

    save

---
+++ UDM/JS/SC002/07/js/dist/script.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/*
+alert("Mensagem");
+confirm("Confirmação");
+prompt("Nome: ")
+*/
+var num1 = prompt("Numero");
+var num2 = prompt("Outro Numero");
--- UDM/JS/SC002/07/js/script.js
+++ UDM/JS/SC002/07/js/script.js
@@ -1,3 +1,8 @@
+/*
 alert("Mensagem");
 confirm("Confirmação");
 prompt("Nome: ")
+*/
+let num1 = prompt("Numero");
+
+let num2 = prompt("Outro Numero");
