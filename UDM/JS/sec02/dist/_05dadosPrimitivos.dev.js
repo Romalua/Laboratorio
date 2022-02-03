e466ee32a572f4e9fc98f389b1b64f99f6ae5a07
Romalua <72881157+Romalua@users.noreply.github.com>
2022-02-03 08:54:11 -0300

    save

--- UDM/JS/sec02/dist/_05dadosPrimitivos.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-//string 'nome'  "nome" `nome`
-// number 10 10.52 
-var nomeAluno; //undefined = não aponta pra lugar nenhum
-
-var sobreAluno = null; //não aponta pra lugar nenhum
-
-var aprovado = False; // Boolean = True, False (lógico)
