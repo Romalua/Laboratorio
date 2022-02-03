514433ebf58deef47387c71e4499dae04829e215
Romalua <72881157+Romalua@users.noreply.github.com>
2022-02-03 09:46:06 -0300

    save

--- UDM/JS/SC002/07/js/dist/script.dev.js
+++ UDM/JS/SC002/07/js/dist/script.dev.js
@@ -5,5 +5,7 @@ alert("Mensagem");
 confirm("Confirmação");
 prompt("Nome: ")
 */
-var num1 = prompt("Numero");
-var num2 = prompt("Outro Numero");
+var num1 = prompt("Numero: ");
+var num2 = prompt("Outro Numero: ");
+num1 = parseInt(num1);
+num2 = parseInt(num2);
--- UDM/JS/SC002/07/js/script.js
+++ UDM/JS/SC002/07/js/script.js
@@ -3,6 +3,8 @@ alert("Mensagem");
 confirm("Confirmação");
 prompt("Nome: ")
 */
-let num1 = prompt("Numero");
+let num1 = prompt("Numero: ");
+let num2 = prompt("Outro Numero: ");
 
-let num2 = prompt("Outro Numero");
+num1 = parseInt(num1);
+num2 = parseInt(num2);
