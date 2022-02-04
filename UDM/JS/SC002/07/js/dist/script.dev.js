fcd2b4ddc938b0737738813cb50340fe819bb562
Romalua <72881157+Romalua@users.noreply.github.com>
2022-02-04 08:57:10 -0300

    save

--- UDM/JS/SC002/07/js/dist/script.dev.js
+++ UDM/JS/SC002/07/js/dist/script.dev.js
@@ -9,5 +9,6 @@ var num1 = prompt("Numero: ");
 var num2 = prompt("Outro Numero: ");
 num1 = Number(num1);
 num2 = Number(num2);
-var resultado = num1 + num2;
-alert("O resultado \xE9 ".concat(resultado));
+var resultado = num1 * num2;
+alert("O resultado  ".concat(resultado));
+alert("Multiplicando ".concat(num1, " por ").concat(num2, " resulta em ").concat(resultado));
--- UDM/JS/SC002/07/js/script.js
+++ UDM/JS/SC002/07/js/script.js
@@ -9,6 +9,7 @@ let num2 = prompt("Outro Numero: ");
 num1 = Number(num1);
 num2 = Number(num2);
 
-const resultado = num1 + num2;
+const resultado = num1* num2;
 
-alert(`O resultado é ${resultado}`);
+alert(`O resultado  ${resultado}`);
+alert(`Multiplicando ${num1} por ${num2} resulta em ${resultado}`)
