138d8335823e89f697296ae05ea804a91af32f6e
Romalua <72881157+Romalua@users.noreply.github.com>
2022-02-07 11:41:46 -0300

    save

---
+++ UDM/JS/SC002/07/js/dist/script2.dev.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var nome = prompt("Seu nome: ");
+var nasc = prompt("Data de nascimento: ");
+var idade = 2022 - nasc;
+alert("Prazer ".concat(nome, " sua idade ").concat(idade));
---
+++ UDM/JS/SC002/07/js/script2.js
@@ -0,0 +1,5 @@
+const nome = prompt("Seu nome: ");
+const nasc = prompt("Data de nascimento: ");
+const idade = (2022 - nasc);
+
+alert(`Prazer ${nome} sua idade ${idade}`)
