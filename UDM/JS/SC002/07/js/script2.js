818058a7c063c772f68bc4d182ff11e8b4e921e4
Romalua <72881157+Romalua@users.noreply.github.com>
2022-02-08 10:45:37 -0300

    save

--- UDM/JS/SC002/07/js/script2.js
+++ UDM/JS/SC002/07/js/script2.js
@@ -2,4 +2,4 @@ const nome = prompt("Seu nome: ");
 const nasc = prompt("Data de nascimento: ");
 const idade = (2022 - nasc);
 
-alert(`Prazer ${nome} sua idade ${idade}`)
+alert(`Prazer ${nome} sua idade ${idade}`)  
