1af3ce735f7aeb6dbd77241953ad9fea22782c1b
Romalua <72881157+Romalua@users.noreply.github.com>
2022-02-09 09:33:44 -0300

    save

---
+++ UDM/JS/SC002/08/dist/variaveis.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var varA = 'B'; //B
+
+var varB = 'C'; //C
+
+var varC = 'A'; //A
+
+varA = varB;
+varC = console.log(varA, varB, varC);
---
+++ UDM/JS/SC002/08/variaveis.js
@@ -0,0 +1,9 @@
+let varA = 'B'; //B
+let varB = 'C'; //C
+let varC = 'A'; //A
+
+varA = varB;
+varC = 
+
+
+console.log(varA, varB, varC);
